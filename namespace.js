56279422f45afb276eb36218dd57bec425a2edd4
nola0123 <43067817+nola0123@users.noreply.github.com>
2018-10-26 14:50:45 -0400

    Create namespace.js

---
+++ namespace.js
@@ -0,0 +1,35 @@
+/***************************
+Filename: namespace.js
+Author: @Denis Nolasco Jovel
+Description: namespace assignment
+Date: 10/26/18
+***************************/
+
+var NOLA0123 = {
+    
+    init: function () {
+        
+        let newDiv = document.createElement("div");
+        newDiv.className = ("box");
+        newDiv.textContent = ("nola0123");
+        document.getElementById("boxes").appendChild(newDiv);
+
+
+        newDiv.addEventListener("mouseover", function () {
+            newDiv.classList.toggle("highlight");
+            
+        });
+
+        newDiv.addEventListener("mouseout", function () {
+            newDiv.classList.toggle("highlight");
+            newDiv.removeAttribute("style");
+            newDiv.textContent = "nola0123";
+        });
+
+        newDiv.addEventListener("click", function () {
+            newDiv.style.borderColor = "blue";
+            newDiv.style.backgroundColor = "yellow";
+            newDiv.textContent = "Ma name Jeff";
+        });
+    }
+}
